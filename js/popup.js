60adf694182d265b3495ab8b40d8f29b4cab255f
Terrence Wells <terrencewells47@gmail.com>
2018-02-28 00:21:57 -0500

    Popup Whitelist

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,43 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+//Whitelisting Popup Logic. Uncommented for right now.
+$( document ).ready(function() {
+
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+      var url = tabs[0].url;
+      console.log("current url: " + url)
+
+  chrome.storage.sync.get("whitelist", function (obj) {
+
+    if($.inArray(url, obj.whitelist) > -1) {
+
+      document.getElementById("white").textContent="Remove from Whitelist";
+
+    }
+
+  document.getElementById("white").addEventListener("click", function(){
+
+          if(obj.whitelist.length < 1) {
+            chrome.storage.sync.set({"whitelist": [url]})
+          } else if($.inArray(url, obj.whitelist) <= -1) {
+            var arr = obj.whitelist;
+
+            arr.push(url)
+
+            console.log(arr)
+
+            console.log($.inArray(url, arr) > -1)
+
+            chrome.storage.sync.set({"whitelist": arr})
+          } else {
+            alert("Already Whitelisted")
+          }
+
+
+
+    });
+
+    });
+
+  });
+
+
+});
